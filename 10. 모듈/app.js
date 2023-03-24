5736f021c04226c62b712c5e30871b3c0781ec70
Soon9 (SoonGu Hong) <naong526@naver.com>
2023-03-24 16:30:04 +0900

    updown게임 ui

---
+++ 10. 모듈/app.js
@@ -0,0 +1,13 @@
+
+// 외부 js 불러오기
+import * as foo from './foo.js';
+import * as bar from './bar.js';
+
+
+// 메인 실행 파일 app.js
+// 실행 코드만 입력: 함수 호출
+// 함수 정의, 클래스 정의, 변수정의 XXXXX
+
+console.log(foo.add(10, 20));
+console.log(`x1: ${bar.x}`);
+console.log(`x2: ${foo.x}`);
---
+++ UPDOWN/js/app.js
@@ -0,0 +1,10 @@
+
+import gameStart from "./start.js";
+
+//========================================//
+// 실행부
+(function() {
+    
+    gameStart(100);
+
+})();
